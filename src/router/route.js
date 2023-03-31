a5fdd2683c09c37ed33b966031ba63643d2c38ed
ongomobile <ongomobile@gmail.com>
2023-03-30 18:00:07 -0700

    Add routes & supporting files

--- src/router/route.js
+++ src/router/route.js
@@ -1,16 +1,141 @@
+import auth from '@/middleware/auth';
+import guest from '@/middleware/guest';
 const routes = [
   {
-    path: "/",
-    name: "Layout",
-    redirect: "/home",
-    component: () => import("@/Layout/index.vue"),
+    path: '/',
+    name: 'Login',
+    component: () => import('@/views/auth/login/index.vue'),
+  },
+  {
+    path: '/register',
+    name: 'reg',
+    component: () => import('@/views/auth/register'),
+  },
+  {
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: () => import('@/views/auth/forgot-password.vue'),
+  },
+  {
+    path: '/lock-screen',
+    name: 'lock-screen',
+    component: () => import('@/views/auth/lock-screen.vue'),
+  },
+  {
+    path: '/success-500',
+    name: 'success-500',
+    component: () => import('@/views/auth/success.vue'),
+  },
+  {
+    path: '/app',
+    name: 'Layout',
+    redirect: '/app/home',
+    component: () => import('@/Layout/index.vue'),
+    meta: {
+      middleware: [auth],
+    },
     children: [
       {
-        path: "/home",
-        name: "home",
-        component: () => import("@/views/index.vue"),
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/home/index.vue'),
+        meta: {
+          hide: true,
+        },
+      },
+      {
+        path: 'invoice',
+        name: 'invoice',
+        component: () => import('@/views/utility/invoice'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'invoice-preview',
+        name: 'invoice-preview',
+        component: () => import('@/views/utility/invoice/invoice-preview'),
+        meta: {
+          hide: true,
+        },
+      },
+      {
+        path: 'invoice-edit',
+        name: 'invoice-edit',
+        component: () => import('@/views/utility/invoice/invoice-edit'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'invoice-add',
+        name: 'invoice-add',
+        component: () => import('@/views/utility/invoice/invoice-add'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'Pricing',
+        name: 'pricing',
+        component: () => import('@/views/utility/pricing'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'faq',
+        name: 'faq',
+        component: () => import('@/views/utility/faq'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'blog',
+        name: 'blog',
+        component: () => import('@/views/utility/blog'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'blog-details',
+        name: 'blog-details',
+        component: () => import('@/views/utility/blog/blog-details'),
+        meta: {
+          groupParent: 'Utility',
+        },
+      },
+      {
+        path: 'testimonial',
+        name: 'testimonial',
+        component: () => import('@/views/utility/testimonial'),
+        meta: {
+          groupParent: 'Utility',
+        },
       },
     ],
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: '404',
+    component: () => import('@/views/404.vue'),
+  },
+  {
+    path: '/coming-soon',
+    name: 'coming-soon',
+    component: () => import('@/views/utility/comming-soon'),
+  },
+  {
+    path: '/under-construction',
+    name: 'under-construction',
+    component: () => import('@/views/utility/under-construction'),
+  },
+  {
+    path: '/error',
+    name: 'error',
+    component: () => import('@/views/error.vue'),
   },
 ];
 

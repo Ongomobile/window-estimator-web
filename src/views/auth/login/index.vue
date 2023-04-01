540d02b8e8a608ed82475eaa4d235d157cf38e70
ongomobile <ongomobile@gmail.com>
2023-03-31 17:23:49 -0700

    Add icons and styles

--- src/views/auth/login/index.vue
+++ src/views/auth/login/index.vue
@@ -31,18 +31,6 @@
               <div class="text-slate-500 text-base">Sign in to your account to start using Dashcode</div>
             </div>
             <Signin />
-            <!-- <div
-              class="relative border-b-[#9AA2AF] border-opacity-[16%] border-b pt-6"
-            >
-              <div
-                class="absolute inline-block bg-white dark:bg-slate-800 dark:text-slate-400 left-1/2 top-1/2 transform -translate-x-1/2 px-4 min-w-max text-sm text-slate-500 font-normal"
-              >
-                Or continue with
-              </div>
-            </div>
-            <div class="max-w-[242px] mx-auto mt-8 w-full">
-              <Social />
-            </div> -->
             <div
               class="md:max-w-[345px] mx-auto font-normal text-slate-500 dark:text-slate-400 mt-12 uppercase text-sm"
             >

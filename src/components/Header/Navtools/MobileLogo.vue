b4acae8536ec0b21cdaa1a86ad812d0dfca0fdc7
ongomobile <ongomobile@gmail.com>
2023-03-31 19:10:14 -0700

    Remove update mobile logo

--- src/components/Header/Navtools/MobileLogo.vue
+++ src/components/Header/Navtools/MobileLogo.vue
@@ -1,16 +1,8 @@
 <template>
   <router-link :to="{ name: 'home' }">
-    <img
-      src="@/assets/images/logo/logo-c.svg"
-      alt=""
-      v-if="!this.$store.themeSettingsStore.isDark"
-    />
+    <img src="@/assets/images/logo/logo.svg" alt="" v-if="!this.$store.themeSettingsStore.isDark" />
 
-    <img
-      src="@/assets/images/logo/logo-c-white.svg"
-      alt=""
-      v-if="this.$store.themeSettingsStore.isDark"
-    />
+    <img src="@/assets/images/logo/logo-white.svg" alt="" v-if="this.$store.themeSettingsStore.isDark" />
   </router-link>
 </template>
 <script></script>

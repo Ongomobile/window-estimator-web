ecf1b4f0b4b3497a37ff798e1113a61328530cf9
ongomobile <ongomobile@gmail.com>
2023-04-02 10:41:57 -0700

    Refactor to use useAppCheck function

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-//import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -15,13 +11,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-
-// // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// // key is the counterpart to the secret key you set in the Firebase console.
-// export const appCheck = initializeAppCheck(app, {
-//   provider: new ReCaptchaV3Provider('6Ldyn1ElAAAAAOnVO8L6xmKBIlaRREW137F0iCpW'),
-
-//   // Optional argument. If true, the SDK automatically refreshes App Check
-//   // tokens as needed.
-//   isTokenAutoRefreshEnabled: true,
-// });
--- src/main.js
+++ src/main.js
@@ -28,8 +28,9 @@ const app = createApp(App)
   .use(VueFire, {
     // imported above but could also just be created here
     firebaseApp,
-    modules: [VueFireAuth(), useAppCheck('6Ldyn1ElAAAAAOnVO8L6xmKBIlaRREW137F0iCpW')],
+    modules: [VueFireAuth()],
   })
+  .useAppCheck('6Ldyn1ElAAAAAOnVO8L6xmKBIlaRREW137F0iCpW')
   .use(pinia)
   .use(VueSweetalert2)
   .use(Toast, {

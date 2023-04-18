7740559ec18846b289e0db6dcc644ef7eab0cc53
ongomobile <ongomobile@gmail.com>
2023-04-18 15:56:16 -0700

    Refactor login to use pinia store

--- src/views/auth/common/Signin.vue
+++ src/views/auth/common/Signin.vue
@@ -3,7 +3,6 @@
     <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
       <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
         <h1 class="mb-8 text-3xl text-center">Sign In</h1>
-
         <form action="">
           <Textinput
             label="Email"
@@ -33,21 +32,38 @@
             Sign In
           </button>
           <div class="mt-2">
-            <router-link to="/forgot-password" class="text-sm text-slate-800 dark:text-slate-400 leading-6 font-medium">
+            <router-link
+              to="/forgot-password"
+              class="text-sm text-slate-800 dark:text-slate-400 leading-6 font-medium"
+            >
               Forgot Password?
             </router-link>
           </div>
         </form>
         <div class="text-center text-sm text-grey-dark mt-4">
           By signing up, you agree to the
-          <a class="no-underline border-b border-grey-dark text-grey-dark" href="#"> Terms of Service </a> and
-          <a class="no-underline border-b border-grey-dark text-grey-dark" href="#"> Privacy Policy </a>
+          <a
+            class="no-underline border-b border-grey-dark text-grey-dark"
+            href="#"
+          >
+            Terms of Service
+          </a>
+          and
+          <a
+            class="no-underline border-b border-grey-dark text-grey-dark"
+            href="#"
+          >
+            Privacy Policy
+          </a>
         </div>
       </div>
 
       <div class="text-grey-dark mt-6">
         Already have an account?
-        <router-link to="/register" class="text-slate-900 dark:text-white font-medium hover:underline">
+        <router-link
+          to="/register"
+          class="text-slate-900 dark:text-white font-medium hover:underline"
+        >
           Sign Up</router-link
         >
       </div>
@@ -55,17 +71,12 @@
   </div>
 </template>
 <script setup>
-import { useFirebaseAuth } from 'vuefire';
-import { signInWithEmailAndPassword } from '@firebase/auth';
+import { useStoreAuth } from '@/store/storeAuth';
 import { useField, useForm } from 'vee-validate';
 import * as yup from 'yup';
-import { useRouter } from 'vue-router';
-import { useToast } from 'vue-toastification';
 import Textinput from '@/components/Textinput';
 
-const toast = useToast();
-const router = useRouter();
-const auth = useFirebaseAuth();
+const storeAuth = useStoreAuth();
 
 const schema = yup.object({
   email: yup.string().required('Email is required').email(),
@@ -81,38 +92,6 @@ const { value: email, errorMessage: emailError } = useField('email');
 const { value: password, errorMessage: passwordError } = useField('password');
 
 const signInToFirebase = handleSubmit((values) => {
-  signInWithEmailAndPassword(auth, values.email, values.password)
-    .then((userCredential) => {
-      // Signed in
-
-      const user = userCredential.user;
-      router.push('/app/home');
-      toast.success(' Login  successfull', {
-        timeout: 2000,
-      });
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-
-      switch (error.code) {
-        case 'auth/wrong-password':
-          toast.error('Incorrect Password', {
-            timeout: 2000,
-          });
-          break;
-        case 'auth/user-not-found':
-          toast.error('No user found with that email', {
-            timeout: 2000,
-          });
-          break;
-        default:
-          toast.error('Sorry, there was an unexpected error', {
-            timeout: 2000,
-          });
-      }
-
-      console.log(errorCode, errorMessage);
-    });
+  storeAuth.loginUser(values);
 });
 </script>

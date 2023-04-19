b1c23fa7dae4f3410ce156e05468908734c5a928
ongomobile <ongomobile@gmail.com>
2023-04-18 21:46:53 -0700

    Update file path

--- src/views/app/counters/index.vue
+++ src/views/app/counters/index.vue
@@ -14,7 +14,7 @@
 
 <script setup>
 import Button from '@/components/Button';
-import AddCounter from './addCounter';
+import AddCounter from '@views/app/counters/addCounter';
 import { useStoreCounters } from '@/store/storeCounters';
 
 const storeCounters = useStoreCounters();

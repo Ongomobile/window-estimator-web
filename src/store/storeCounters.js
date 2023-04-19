cd04a6421d8558148ea44f7d27edcff9e7caf044
ongomobile <ongomobile@gmail.com>
2023-04-18 18:56:51 -0700

    Start counters store

---
+++ src/store/storeCounters.js
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia';
+
+export const useCountersStore = defineStore('countersStore', {
+  state: () => {
+    return {
+      counters: [],
+      addModal: false,
+      editModal: false,
+    };
+  },
+  actions: {
+    async getCounters() {
+      // get counters
+    },
+    async addCounter() {
+      // add counter
+    },
+    async deleteCounter() {
+      // delete counter
+    },
+    async updateCounter() {
+      // update counter
+    },
+    clearNotes() {
+      this.counters = [];
+      // if (getNotesSnapshot) getNotesSnapshot(); // unsubscribe from any active listener
+    },
+    openCounterModal() {
+      this.addModal = true;
+    },
+    closeCounterModal() {
+      this.addModal = false;
+    },
+  },
+});

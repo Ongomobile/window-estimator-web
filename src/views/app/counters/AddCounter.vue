7a5520f1fa2548681b85cc8dbe70a6ef53159bed
ongomobile <ongomobile@gmail.com>
2023-04-18 21:18:14 -0700

    Update styles

--- src/views/app/counters/AddCounter.vue
+++ src/views/app/counters/AddCounter.vue
@@ -2,12 +2,10 @@
   <Modal
     :activeModal="addModal"
     @close="closeAddModal"
-    title="Add Counter"
+    title="Add A Counter"
     centered
   >
     <div class="add-counter-form--wrapper">
-      <h2 class="add-counter-heading">Add A Counter</h2>
-      <!-- prevent modifier prevents default -->
       <form
         class="add-counter-form"
         @submit.prevent="addCounter"
@@ -55,7 +53,7 @@
           placeholder="Add Window Price"
         />
         <SelectImage
-          :images="windowImages"
+          :images="windowData"
           @on-select-image="selectImage"
         />
         <input
@@ -76,160 +74,17 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, reactive } from 'vue';
 import Modal from '@/components/Modal';
 import { useStoreCounters } from '@/store/storeCounters';
 import SelectImage from '@/views/app/counters/SelectImage.vue';
-import windowData from './windowData.json';
+import windowData from './windowData';
 
 const storeCounters = useStoreCounters();
 
 const addModal = computed(() => storeCounters.addModal);
 const imageUrl = ref('');
 const alt = ref('');
-
-const windowImages = [
-  {
-    id: 0,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/singlePane125x125_bwvnor.png',
-    name: 'Single Pane',
-    alt: 'Single Pane',
-    selected: false,
-  },
-  {
-    id: 1,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/2pane125x125_ixucil.png',
-    name: 'Two Panes',
-    alt: 'Two Panes',
-    selected: false,
-  },
-  {
-    id: 2,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/3pane125x125_wpdngh.png',
-    name: 'Three Panes',
-    alt: 'Three Panes',
-    selected: false,
-  },
-  {
-    id: 3,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/10paneFrenchDoor125x125_vjsp4n.png',
-    name: 'French Door',
-    alt: 'French Door',
-    selected: false,
-  },
-  {
-    id: 4,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/12paneFrench125x125_c93sk2.png',
-    name: 'French Window',
-    alt: 'French Window',
-    selected: false,
-  },
-  {
-    id: 5,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/frostedBathroomWindows_nrheof.png',
-    name: 'Frosted Bathroom',
-    alt: 'Frosted Bathroom',
-    selected: false,
-  },
-  {
-    id: 6,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/gardenWindow125x125_ryqide.png',
-    name: 'Garden Window',
-    alt: 'Garden Window',
-    selected: false,
-  },
-  {
-    id: 7,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641844853/glassDoors125x125_oxkd1s.png',
-    name: 'Glass Door',
-    alt: 'Glass Door',
-    selected: false,
-  },
-  {
-    id: 8,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/Inside-ladder_2x_yyzcuz.png',
-    name: 'Ladder Work',
-    alt: 'Ladder Work',
-    selected: false,
-  },
-  {
-    id: 9,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/largePane125x125_ndq5vj.png',
-    name: 'Large Pane',
-    alt: 'Large Pane',
-    selected: false,
-  },
-  {
-    id: 10,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788053/multiPane125x125_n91dtf.png',
-    name: 'Multi Pane',
-    alt: 'Multi Pane',
-    selected: false,
-  },
-  {
-    id: 11,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1642190642/Storm-window_m2oxyp.png',
-    name: 'Storm Window',
-    alt: 'Storm Window',
-    selected: false,
-  },
-  {
-    id: 12,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/screen125x125_et506p.png',
-    name: 'Screens',
-    alt: 'Screens',
-    selected: false,
-  },
-  {
-    id: 13,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/shapedWindows125x125_hwfiqd.png',
-    name: 'Shaped Windows',
-    alt: 'Shaped Windows',
-    selected: false,
-  },
-  {
-    id: 14,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/slidingDoor125x125_gkngmd.png',
-    name: 'Sliding Doors',
-    alt: 'Sliding Doors',
-    selected: false,
-  },
-  {
-    id: 15,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/skylights125x125_na0uaj.png',
-    name: 'Sky Lights',
-    alt: 'Sky Lights',
-    selected: false,
-  },
-  {
-    id: 16,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/wagonWheel125x125_xmw7ve.png',
-    name: 'Wagon Wheel',
-    alt: 'Wagon Wheel',
-    selected: false,
-  },
-  {
-    id: 17,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641788054/tracks125x125_zuttvg.png',
-    name: 'Window Tracks',
-    alt: 'Window Tracks',
-    selected: false,
-  },
-  {
-    id: 18,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641845079/razorBlade125x125_cupci5.png',
-    name: 'Construction',
-    alt: 'Construction',
-    selected: false,
-  },
-  {
-    id: 19,
-    url: 'https://res.cloudinary.com/dnpje4e34/image/upload/v1641851671/Default-img_cntbq2.png',
-    name: 'Default Image',
-    alt: 'Default Image',
-    selected: false,
-  },
-];
 
 const closeAddModal = () => {
   storeCounters.closeCounterModal();
@@ -240,12 +95,11 @@ let selectImage = (imageData) => {
   alt = imageData.alt;
 };
 </script>
+
 <style>
 .add-counter-form--wrapper {
   display: flex;
   flex-direction: column;
-  max-width: 450px;
-  /* margin: 0 auto 30px; */
 }
 
 .add-counter-form {
@@ -257,12 +111,6 @@ let selectImage = (imageData) => {
   margin-bottom: 10px;
   box-shadow: var(--box-shadow);
   background-color: #fff;
-}
-
-.add-counter-heading {
-  margin-bottom: 5px;
-  text-align: center;
-  color: rgb(71 85 105);
 }
 
 .add-counter-input {

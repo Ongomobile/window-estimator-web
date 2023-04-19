61a8f508e6638ba2731c9ee57e12c2c215ccaa40
ongomobile <ongomobile@gmail.com>
2023-04-19 09:49:48 -0700

    Refactor to use add edit form

--- src/components/Counters/AddCounter.vue
+++ src/components/Counters/AddCounter.vue
@@ -5,95 +5,24 @@
     title="Add A Counter"
     centered
   >
-    <div class="add-counter-form--wrapper">
-      <form
-        class="add-counter-form"
-        @submit.prevent="addCounter"
-      >
-        <label
-          for="type"
-          class="add-counter-label"
-          title="add"
-          >Window Type</label
-        >
-        <input
-          type="text"
-          id="type"
-          name="type"
-          class="add-counter-input"
-          placeholder="Add Window Type"
-          v-model="windowType"
-        />
-        <label
-          for="location"
-          class="add-counter-label"
-          >Window Location</label
-        >
-        <input
-          type="text"
-          id="location"
-          name="location"
-          class="add-counter-input"
-          placeholder="Add Location (default in & out)"
-          v-model="windowLocation"
-        />
-
-        <label
-          for="price"
-          class="add-counter-label"
-          >Window Price</label
-        >
-        <!-- Use .number modifier to cast input value as number -->
-        <input
-          type="text"
-          id="price"
-          name="price"
-          class="add-counter-input"
-          v-model.number="windowPrice"
-          placeholder="Add Window Price"
-        />
-        <SelectImage
-          :images="windowData"
-          @on-select-image="selectImage"
-        />
-        <input
-          type="submit"
-          class="add-counter-input submit"
-          value="Add Counter"
-          :disabled="isDisabled"
-        />
-        <div
-          class="error-message-wrapper"
-          v-if="validated"
-        >
-          <p class="error-message">* type & price are required *</p>
-        </div>
-      </form>
-    </div>
+    <AddEditCounter />
   </Modal>
 </template>
-
 <script setup>
-import { ref, computed, reactive } from 'vue';
+import { computed } from 'vue';
 import Modal from '@/components/Modal';
 import { useStoreCounters } from '@/store/storeCounters';
-import SelectImage from '@/components/Counters/SelectImage.vue';
-import windowData from './windowData';
+import AddEditCounter from '@/components/Counters/AddEditCounter';
 
 const storeCounters = useStoreCounters();
 
 const addModal = computed(() => storeCounters.addModal);
-const imageUrl = ref('');
-const alt = ref('');
 
 const closeAddModal = () => {
   storeCounters.closeCounterModal();
 };
 
-let selectImage = (imageData) => {
-  imageUrl = imageData.url;
-  alt = imageData.alt;
-};
+const addCounter = () => {};
 </script>
 
 <style>
---
+++ src/components/Counters/AddEditCounter.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="add-counter-form--wrapper">
+    <form
+      class="add-counter-form"
+      @submit.prevent="addCounter"
+    >
+      <label
+        for="type"
+        class="add-counter-label"
+        title="add"
+        >Window Type</label
+      >
+      <input
+        type="text"
+        id="type"
+        name="type"
+        class="add-counter-input"
+        placeholder="Add Window Type"
+        v-model="windowType"
+      />
+      <label
+        for="location"
+        class="add-counter-label"
+        >Window Location</label
+      >
+      <input
+        type="text"
+        id="location"
+        name="location"
+        class="add-counter-input"
+        placeholder="Add Location (default in & out)"
+        v-model="windowLocation"
+      />
+
+      <label
+        for="price"
+        class="add-counter-label"
+        >Window Price</label
+      >
+      <!-- Use .number modifier to cast input value as number -->
+      <input
+        type="text"
+        id="price"
+        name="price"
+        class="add-counter-input"
+        v-model.number="windowPrice"
+        placeholder="Add Window Price"
+      />
+      <SelectImage
+        :images="windowData"
+        @on-select-image="selectImage"
+      />
+      <input
+        type="submit"
+        class="add-counter-input submit"
+        value="Add Counter"
+        :disabled="isDisabled"
+      />
+      <div
+        class="error-message-wrapper"
+        v-if="validated"
+      >
+        <p class="error-message">* type & price are required *</p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+import { useStoreCounters } from '@/store/storeCounters';
+import SelectImage from '@/components/Counters/SelectImage.vue';
+import windowData from './windowData';
+
+const storeCounters = useStoreCounters();
+
+let selectImage = (imageData) => {
+  imageUrl = imageData.url;
+  alt = imageData.alt;
+};
+
+const counterData = reactive({
+  windowPrice: '',
+  windowType: '',
+  windowLocation: '',
+  imageUrl: '',
+  alt: '',
+  quantity: 0,
+  counterId: 0,
+  subtotal: null,
+});
+
+const addCounter = () => {};
+</script>
